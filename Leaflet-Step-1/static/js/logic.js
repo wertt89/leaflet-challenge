a35c98053cd9af1f868b5da652e22833c0531dc1
Konsta Jokipii <konstajokipii@Konstas-MacBook-Pro.local>
2021-06-02 22:40:47 -0500

    pushing step 1

---
+++ Leaflet-Step-1/static/js/logic.js
@@ -0,0 +1,90 @@
+// Defining earthquake plates GeoJSON url variable
+var earthquakeURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+// Getting request for data
+d3.json(earthquakeURL, function(data) {
+    createFeatures(data.features);
+});
+// Defining function to run "onEach" feature 
+function createFeatures(earthquakeData) {
+    var earthquakes = L.geoJSON(earthquakeData, {
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup("<h3>Magnitude: " + feature.properties.mag +"</h3><h3>Location: "+ feature.properties.place +
+              "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+          },
+
+          pointToLayer: function (feature, latlng) {
+            return new L.circle(latlng,
+              {radius: getRadius(feature.properties.mag),
+              fillColor: getColor(feature.properties.mag),
+              fillOpacity: .5,
+              color: "black",
+              stroke: true,
+              weight: .8
+          })
+        }
+        });
+
+    createMap(earthquakes);
+}
+
+function createMap(earthquakes) {
+
+    // Defining map layer
+    var lightMap = L.tileLayer("https://api.mapbox.com/styles/v1/mfatih72/ck30rkku519fu1drmiimycohl/tiles/256/{z}/{x}/{y}?" + 
+    "access_token=pk.eyJ1Ijoia29uc3RhaiIsImEiOiJja3BlcHg0YmEwM3hjMnZxeHlmd2ZrY29rIn0.gbhyzmTECW5NbhJS2wPONg");
+    
+      // Defining base map
+    var baseMaps = {
+        "LightMap": lightMap,
+    };
+
+    // Creating map
+    var myMap = L.map("map", {
+        center: [40.7, -94.5],
+        zoom: 5,
+        layers: [lightMap, earthquakes]
+    });
+
+    // Creating legend
+    var legend = L.control({
+        position: "bottomleft"
+    });
+
+    legend.onAdd = function(myMap) {
+        var div = L.DomUtil.create("div", "info legend"),
+        grades = [0, 1, 2, 3, 4, 5],
+        labels = [];
+
+    // Creating legend
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+    return div;
+    };
+    legend.addTo(myMap);
+}
+
+// Creating coloring logic
+function getColor(magnitude) {
+    if (magnitude > 5) {
+        return 'red'
+    } else if (magnitude > 4) {
+        return 'orange'
+    } else if (magnitude > 3) {
+        return 'yellow'
+    } else if (magnitude > 2) {
+        return 'lightgreen'
+    } else if (magnitude > 1) {
+        return 'green'
+    } else {
+        return 'magenta'
+    }
+};
+
+//Creating radius function
+function getRadius(magnitude) {
+    return magnitude * 20000;
+};
